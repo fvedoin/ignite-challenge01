36cabc3c2c964162f61bf78014ad8ef6da0411f4
Fernando <fernando.vgarcia@hotmail.com>
2021-09-17 22:37:41 -0300

    Setting tasks as completed.

--- src/components/TaskList.tsx
+++ src/components/TaskList.tsx
@@ -21,7 +21,14 @@ export function TaskList() {
   }
 
   function handleToggleTaskCompletion(id: number) {
-    // Altere entre `true` ou `false` o campo `isComplete` de uma task com dado ID
+    setTasks(tasks.map(task => {
+      if(task.id === id){
+        task.isComplete = !task.isComplete;
+        return task;
+      }else{
+        return task;
+      }
+    }));
   }
 
   function handleRemoveTask(id: number) {
